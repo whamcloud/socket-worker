33205eebe3cbc70f8604bf62d428bb90b998564c
Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>
2015-10-22 17:27:33 -0400

    HYD-5030 Add build script to socket-worker
    We need the ability to build socket-worker
    from the GUI with correct paths for
    browserify and minifyify.
    
    Add a simple node script that can be required
    by the GUI in it's gulpfile.
    
     - Ignored .idea.
     - Added the build script.
     - Updated package.json.
    
    Signed-off-by: Wayne Kenneth Seymour III <wayne.k.seymour.iii@intel.com>

---
+++ build.js
@@ -0,0 +1,14 @@
+var join = require('path').join;
+var browserify = require('browserify');
+
+module.exports = function buildSocketWorker (outDir) {
+  var srcMapName = 'bundle.map.json';
+  var minifyifyDest = join(outDir, srcMapName);
+  var socketWorkerIndex = join(__dirname, 'index.js');
+
+  return browserify(socketWorkerIndex, { debug: true })
+    .plugin('minifyify', { minify: true,  map: srcMapName, output: minifyifyDest })
+    .bundle(function handleErrors (err) {
+      if (err) throw err;
+    });
+};
