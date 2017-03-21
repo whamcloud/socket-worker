d62117e4269457d3c9369bdde9a690f3664700dc
Will Johnson <william.c.johnson@intel.com>
2017-03-21 17:47:20 -0400

    Added date module and tests

--- source/date.js
+++ source/date.js
@@ -1,30 +1,80 @@
-export const getServerMoment = SERVER_TIME_DIFF => {
-  const d = new Date();
+// @flow
 
+//
+// INTEL CONFIDENTIAL
+//
+// Copyright 2013-2017 Intel Corporation All Rights Reserved.
+//
+// The source code contained or described herein and all documents related
+// to the source code ("Material") are owned by Intel Corporation or its
+// suppliers or licensors. Title to the Material remains with Intel Corporation
+// or its suppliers and licensors. The Material contains trade secrets and
+// proprietary and confidential information of Intel or its suppliers and
+// licensors. The Material is protected by worldwide copyright and trade secret
+// laws and treaty provisions. No part of the Material may be used, copied,
+// reproduced, modified, published, uploaded, posted, transmitted, distributed,
+// or disclosed in any way without Intel's prior express written permission.
+//
+// No license under any patent, copyright, trade secret or other intellectual
+// property right is granted to or conferred upon you by disclosure or delivery
+// of the Materials, either expressly, by implication, inducement, estoppel or
+// otherwise. Any license under such intellectual property rights must be
+// express and approved by Intel in writing.
+
+import * as fp from '@iml/fp';
+
+export const getServerMoment = (SERVER_TIME_DIFF:number, date:Date) => {
+  let d = new Date(date.getTime());
   d.setMilliseconds(d.getMilliseconds() + SERVER_TIME_DIFF);
 
   return d;
 };
 
-type Unit = 'hours' | 'days' | 'weeks' | 'months' | 'years';
+export type Unit = 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months' | 'years';
 
 export const adjustDateFromSizeAndUnit = (
   SERVER_TIME_DIFF: number,
-  date: Date,
+  operation: 'add' | 'subtract' = 'subtract',
   size: number,
-  unit: Unit
+  unit: Unit,
+  d: Date,
 ) => {
+  let date = new Date(d.getTime());
+  const calculate = (x, size) => {
+    switch(operation) {
+    case 'add':
+      return x + size;
+    case 'subtract':
+      return x - size;
+    default:
+      throw new Error('must pass in a valid operation.');
+    };
+  };
+
   switch (unit) {
+    case 'seconds':
+      date.setSeconds(calculate(date.getSeconds(), size));
+      break;
+    case 'minutes':
+      date.setMinutes(calculate(date.getMinutes(), size));
+      break;
     case 'hours':
-      date.setHours(date.getHours() - size);
+      date.setHours(calculate(date.getHours(), size));
+      break;
     case 'days':
-      date.getDate(date.getDate() - size);
+      date.setDate(calculate(date.getDate(), size));
+      break;
     case 'weeks':
-      date.getDate(date.getDate() - size * 7);
+      date.setDate(calculate(date.getDate(), size * 7));
+      break;
     case 'months':
-      date.getMonth(date.getMonth - size);
+      date.setMonth(calculate(date.getMonth(), size), date.getDate());
+      break;
     case 'years':
-      date.getFullYear(date.getFullYear - size);
+      date.setFullYear(calculate(date.getFullYear(), size));
+      break;
+    default:
+      throw new Error(`${unit} is not a valid unit.`);
   }
 
   return date;
@@ -33,18 +83,26 @@ export const adjustDateFromSizeAndUnit = (
 export const calculateRangeFromSizeAndUnit = (
   SERVER_TIME_DIFF: number,
   size: number,
-  unit: Unit
+  unit: Unit,
+  date:Date
 ) => {
-  const d = getServerMoment(SERVER_TIME_DIFF);
-  const end = getServerMoment(SERVER_TIME_DIFF).setMilliseconds(0);
+  const adjustDateByOperation = adjustDateFromSizeAndUnit.bind(null, SERVER_TIME_DIFF);
+  const addToDate = adjustDateByOperation.bind(null, 'add');
+  const subtractFromDate = adjustDateByOperation.bind(null, 'subtract');
 
+  let dateClone = new Date(date.getTime());
+  const d = getServerMoment(SERVER_TIME_DIFF, dateClone);
+  let end = new Date(getServerMoment(SERVER_TIME_DIFF, dateClone).setMilliseconds(0));
   const secs = end.getSeconds();
+
   end.setSeconds(secs - secs % 10);
+  end = new Date(addToDate(10, 'seconds', end).toISOString());
 
-  params.qs.end = end.clone().add(10, 'seconds').toISOString();
+  const setBeginTime = fp.flow(
+    subtractFromDate.bind(null, size, unit),
+    subtractFromDate.bind(null, 10, 'seconds')
+  );
+  const start = setBeginTime(end);
 
-  params.qs.begin = end
-    .subtract(size, unit)
-    .subtract(10, 'seconds')
-    .toISOString();
+  return [start, end];
 };
---
+++ source/date.test.js
@@ -0,0 +1,145 @@
+// @flow
+
+import {
+  getServerMoment,
+  adjustDateFromSizeAndUnit,
+  calculateRangeFromSizeAndUnit,
+  type Unit
+} from './date.js';
+
+describe('getServerMoment', () => {
+  let result;
+  it('should return a new date', () => {
+    let d = new Date('2017-12-17T03:24:00');
+    result = getServerMoment(75, d);
+    expect(result.toISOString()).toEqual('2017-12-17T03:24:00.075Z');
+  });
+});
+
+describe('date', () => {
+  let SERVER_TIME_DIFF:number, date:Date, size:number, unit:Unit;
+  let adjustDate:(operation:'add' | 'subtract', size:number, unit:Unit, date:Date) => Date;
+  let subtractDate:(size:number, unit:Unit, date:Date) => Date;
+  let addDate:(size:number, unit:Unit, date:Date) => Date;
+  let result;
+
+  beforeEach(() => {
+    SERVER_TIME_DIFF = 75;
+    date = new Date('2017-12-17T03:24:00');
+    adjustDate = adjustDateFromSizeAndUnit.bind(null, SERVER_TIME_DIFF);
+    subtractDate = adjustDate.bind(null, 'subtract');
+    addDate = adjustDate.bind(null, 'add');
+  });
+
+  it('should subtract seconds and roll back to the previous hour', () => {
+    result = subtractDate(2, 'seconds', date);
+    expect(result.toISOString()).toEqual('2017-12-17T03:23:58.000Z');
+  });
+
+  it('should add seconds to the date', () => {
+    result = addDate(15, 'seconds', date);
+    expect(result.toISOString()).toEqual('2017-12-17T03:24:15.000Z');
+  });
+
+  it('should subtract minutes within the same hour', () => {
+    result = subtractDate(2, 'minutes', date);
+    expect(result.toISOString()).toEqual('2017-12-17T03:22:00.000Z');
+  });
+
+  it('should subtract minutes and rollback to the previous hour', () => {
+    result = subtractDate(25, 'minutes', date);
+    expect(result.toISOString()).toEqual('2017-12-17T02:59:00.000Z');
+  });
+
+  it('should add minutes to the date', () => {
+    result = addDate(15, 'minutes', date);
+    expect(result.toISOString()).toEqual('2017-12-17T03:39:00.000Z');
+  });
+
+  it('should subtract hours within the same date', () => {
+    result = subtractDate(2, 'hours', date);
+    expect(result.toISOString()).toEqual('2017-12-17T01:24:00.000Z');
+  });
+
+  it('should subtract hours and rollback to the previous date', () => {
+    result = subtractDate(7, 'hours', date);
+    expect(result.toISOString()).toEqual('2017-12-16T20:24:00.000Z');
+  });
+
+  it('should add hours to the date', () => {
+    result = addDate(1, 'hours', date);
+    expect(result.toISOString()).toEqual('2017-12-17T04:24:00.000Z');
+  });
+
+  it('should subtract days from the date within the same month', () => {
+    result = subtractDate(2, 'days', date);
+    expect(result.toISOString()).toEqual('2017-12-15T03:24:00.000Z');
+  });
+
+  it('should subtract days from the date and roll back to previous month', () => {
+    result = subtractDate(18, 'days', date);
+    expect(result.toISOString()).toEqual('2017-11-29T03:24:00.000Z');
+  });
+
+  it('should add days to the date', () => {
+    result = addDate(1, 'days', date);
+    expect(result.toISOString()).toEqual('2017-12-18T03:24:00.000Z');
+  });
+
+  it('should subtract weeks from the date within the same month', () => {
+    result = subtractDate(1, 'weeks', date);
+    expect(result.toISOString()).toEqual('2017-12-10T03:24:00.000Z');
+  });
+
+  it('should subtract weeks from the date and roll back to previous month', () => {
+    result = subtractDate(3, 'weeks', date);
+    expect(result.toISOString()).toEqual('2017-11-26T03:24:00.000Z');
+  });
+
+  it('should add weeks to the date', () => {
+    result = addDate(1, 'weeks', date);
+    expect(result.toISOString()).toEqual('2017-12-24T03:24:00.000Z');
+  });
+
+  it('should subtract months from the date within the current year', () => {
+    result = subtractDate(2, 'months', date);
+    // hour changes from 03 to 02 due to DST
+    expect(result.toISOString()).toEqual('2017-10-17T02:24:00.000Z');
+  });
+
+  it('should subtract months from the date and roll back to previous year', () => {
+    result = subtractDate(13, 'months', date);
+    expect(result.toISOString()).toEqual('2016-11-17T03:24:00.000Z');
+  });
+
+  it('should add months to the date', () => {
+    result = addDate(1, 'months', date);
+    expect(result.toISOString()).toEqual('2018-01-17T03:24:00.000Z');
+  });
+
+  it('should subtract years from the date', () => {
+    result = subtractDate(2, 'years', date);
+    expect(result.toISOString()).toEqual('2015-12-17T03:24:00.000Z');
+  });
+
+  it('should add years to the date', () => {
+    result = addDate(1, 'years', date);
+    expect(result.toISOString()).toEqual('2018-12-17T03:24:00.000Z');
+  });
+});
+
+describe('calculate range from size and unit', () => {
+  let date, start, end;
+  beforeEach(() => {
+    date = new Date('2017-12-17T03:24:00');
+    [start, end] = calculateRangeFromSizeAndUnit(75, 10, 'minutes', date);
+  });
+
+  it('should have a start time', () => {
+    expect(start.toISOString()).toEqual('2017-12-17T03:14:00.000Z');
+  });
+
+  it('should have an end time', () => {
+    expect(end.toISOString()).toEqual('2017-12-17T03:24:10.000Z');
+  });
+});
