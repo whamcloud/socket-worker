2a84c285fea94c3e8791ce54d009350a7e20523e
Will Johnson <wjohnson@whamcloud.com>
2018-07-18 12:54:30 -0400

    Update copyright
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/router/middleware/middleware-types.js
+++ source/router/middleware/middleware-types.js
@@ -1,7 +1,7 @@
 // @flow
 
 //
-// Copyright (c) 2017 DDN. All rights reserved.
+// Copyright (c) 2018 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
